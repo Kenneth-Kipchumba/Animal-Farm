131180323b494c026c7d2be0f9338be7d69a8d13
Kenneth Kipchumba <branrono@gmail.com>
2023-01-07 08:04:07 +0300

    Initial commit   - Fresh Laravel 9 installation   - AdminLTE integration (https://virajchorghe.com/2022/07/01/admin-lte-and-laravel-9) with authentication system.

---
+++ database/migrations/2023_01_07_044640_add_initial_user.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        User::create([
+            'first_name' => 'Kenneth',
+            'last_name' => 'Kipchumba',
+            'email' => 'kipchumba.kenneth@ymail.com',
+            'password' => Hash::make('password')
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
